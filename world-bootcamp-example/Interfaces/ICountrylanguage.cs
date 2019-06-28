806aef1d358ad0832ee606296acc598aba972991
mkujun <kujundzic.marko@hotmail.com>
2019-06-28 14:03:51 +0200

    added couple of services for countrylanguage entity

---
+++ world-bootcamp-example/Interfaces/ICountrylanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using world_bootcamp_example.world;
+
+namespace world_bootcamp_example.Interfaces
+{
+    public interface ICountrylanguage
+    {
+        List<Countrylanguage> GetAllCountrylanguages();
+        List<Countrylanguage> GetAllCountrylanguagesByContinent(string continet);
+        List<Countrylanguage> GetAllCountrylanguagesByCountry(string countryName);
+    }
+}
---
+++ world-bootcamp-example/Services/CountrylanguageService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using world_bootcamp_example.Interfaces;
+using world_bootcamp_example.world;
+
+namespace world_bootcamp_example.Services
+{
+    public class CountrylanguageService : ICountrylanguage
+    {
+        private worldContext context;
+
+        public CountrylanguageService(worldContext ctx)
+        {
+            context = ctx;
+        }
+
+        public List<Countrylanguage> GetAllCountrylanguages()
+        {
+            return context.Countrylanguages.ToList();
+        }
+
+        public List<Countrylanguage> GetAllCountrylanguagesByContinent(string continet)
+        {
+            return context.Countrylanguages.Where(c => c.CountryCodeNavigation.Continent == continet).ToList();
+        }
+
+        public List<Countrylanguage> GetAllCountrylanguagesByCountry(string countryName)
+        {
+            return context.Countrylanguages.Where(c => c.CountryCodeNavigation.Name == countryName).ToList();
+        }
+    }
+}
